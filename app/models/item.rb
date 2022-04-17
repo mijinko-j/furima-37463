d7f8d1daa6563365e6834a457bb7d97946fa2fe2
mijinko-j <mijinko415@gmail.com>
2022-04-17 21:11:56 +0900

    商品の状態・配送料の負担・発送元の地域・発送までの日数のActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,13 +7,23 @@ class Item < ApplicationRecord
   validates :prefecture_id,          presence: true
   validates :scheduled_delivery_id,  presence: true
   validates :price,                  presence: true
+  validates :category_id,            numericality: { other_than: 0, message: "can't be blank" }
+  validates :sales_status_id,        numericality: { other_than: 0, message: "can't be blank" }
+  validates :shipping_fee_status_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id,          numericality: { other_than: 0, message: "can't be blank" }
+  validates :scheduled_delivery_id,  numericality: { other_than: 0, message: "can't be blank" }
 
   belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture_id
+  belongs_to :scheduled_delivery_id
 
-  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+  
+  
 end
  

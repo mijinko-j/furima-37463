76002a8b0c992071ba37c9a13f7d3a42af03e0eb
mijinko-j <mijinko415@gmail.com>
2022-04-17 23:23:22 +0900

    バリテーション詳細設定・エラーハンドリング設定・修正など

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.includes(:user)
   end
 
   def new
@@ -8,13 +10,15 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item =Item.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
-      render :new
+      render action: :new
     end
   end
+
+  
   
   private
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  validates :image,                  presence: true
   validates :name,                   presence: true
   validates :info,                   presence: true
   validates :category_id,            presence: true
@@ -6,7 +7,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, presence: true
   validates :prefecture_id,          presence: true
   validates :scheduled_delivery_id,  presence: true
-  validates :price,                  presence: true
+  validates :price,                  presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+
   validates :category_id,            numericality: { other_than: 0, message: "can't be blank" }
   validates :sales_status_id,        numericality: { other_than: 0, message: "can't be blank" }
   validates :shipping_fee_status_id, numericality: { other_than: 0, message: "can't be blank" }
@@ -20,8 +22,8 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
-  belongs_to :prefecture_id
-  belongs_to :scheduled_delivery_id
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 
   
   

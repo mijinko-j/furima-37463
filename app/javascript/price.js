3d20294b16bccff2a0005562e7914278fc5d3ddd
mijinko-j <mijinko415@gmail.com>
2022-04-18 18:01:50 +0900

    販売手数料・販売利益のカンマでの区切り表示追加

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,6 +3,6 @@ window.addEventListener('keyup', function () {
   const itemPriceValue = itemPrice.value;
   const tax = Math.floor(itemPriceValue * 0.1);
   const profit = Math.floor(itemPriceValue - tax);
-  document.getElementById('add-tax-price').innerHTML = tax;
-  document.getElementById('profit').innerHTML = profit;
+  document.getElementById('add-tax-price').innerHTML = tax.toLocaleString();
+  document.getElementById('profit').innerHTML = profit.toLocaleString();
 });

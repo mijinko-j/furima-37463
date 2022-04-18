5fb228b990098b56cba9a49fb75da5eb562bbe22
mijinko-j <mijinko415@gmail.com>
2022-04-18 18:59:11 +0900

    itemモデルの単体テストコード修正v2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Info can't be blank")
       end
       it 'カテゴリーが未選択では登録できない' do
-        @item.category_id = '0'
+        @item.category_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態が未選択では登録できない' do
-        @item.sales_status_id = '0'
+        @item.sales_status_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Sales status can't be blank")
       end
       it '配送料の負担の情報が未選択では登録できない' do
-        @item.shipping_fee_status_id = '0'
+        @item.shipping_fee_status_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
       end
       it '発送元の地域の情報が未選択では登録できない' do
-        @item.prefecture_id = '0'
+        @item.prefecture_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数の情報が未選択では登録できない' do
-        @item.scheduled_delivery_id = '0'
+        @item.scheduled_delivery_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
       end
@@ -58,12 +58,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '価格が¥300未満では登録できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it '価格が¥10,000,000以上では登録できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
@@ -72,8 +72,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it 'ユーザーが紐付いていないとツイートは保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
-
-

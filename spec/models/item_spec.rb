12f197b02518fb75970c52e22c9768555347ca28
mijinko-j <mijinko415@gmail.com>
2022-04-18 18:26:58 +0900

    itemモデルの単体テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Info can't be blank")
       end
       it 'カテゴリーが未選択では登録できない' do
-        @item.category_id = ''
+        @item.category_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態が未選択では登録できない' do
-        @item.sales_status_id = ''
+        @item.sales_status_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Sales status can't be blank")
       end
       it '配送料の負担の情報が未選択では登録できない' do
-        @item.shipping_fee_status_id = ''
+        @item.shipping_fee_status_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
       end
       it '発送元の地域の情報が未選択では登録できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数の情報が未選択では登録できない' do
-        @item.scheduled_delivery_id = ''
+        @item.scheduled_delivery_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
       end

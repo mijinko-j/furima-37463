56e5cd0f8a7e58a86d9173bb67aeaf0481ce4ae3
mijinko-j <mijinko415@gmail.com>
2022-04-18 19:09:05 +0900

    Rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Item, type: :model do
   end
   describe '商品出品' do
     context '商品出品できる場合' do
-      it "必須項目が存在すれば出品できる" do
+      it '必須項目が存在すれば出品できる' do
         expect(@item).to be_valid
       end
     end
@@ -60,22 +60,22 @@ RSpec.describe Item, type: :model do
       it '価格が¥300未満では登録できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '価格が¥10,000,000以上では登録できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it '価格が全角英数字では登録できない' do
         @item.price = '３００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'ユーザーが紐付いていないとツイートは保存できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end

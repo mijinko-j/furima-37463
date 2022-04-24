6d643560f875733a5ff933cb09f2026369da6614
mijinko-j <mijinko415@gmail.com>
2022-04-24 18:28:51 +0900

    売却済み商品の表示変更・ページへのアクセス範囲設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,9 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    if @item.user == current_user
+      redirect_to root_path
+    end
   end
 
   def create

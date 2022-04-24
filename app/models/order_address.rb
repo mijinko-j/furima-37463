ccf6fff95a0b3da247bce6a8c09e4cdbe948ce7a
mijinko-j <mijinko415@gmail.com>
2022-04-24 18:33:54 +0900

    エラーハンドリング重複のため修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,10 +5,10 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true}
     validates :city,         presence: true
     validates :house_number, presence: true
-    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, allow_blank: true}
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   
